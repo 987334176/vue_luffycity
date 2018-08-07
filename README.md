7574938419ae958b7b78e038abe668fb4a0249a4
xiao <383697894@qq.com>
2018-08-07 23:59:13 +0800

    路飞学城

---
+++ README.md
@@ -0,0 +1,40 @@
+## 说明
+路飞学城
+
+webpack + Vue + VueRouter + VueX，使用vue-cli构建项目
+
+## 运行环境
+
+| Project | Status | Description |
+|---------|--------|-------------|
+| node                | 10.7.0 | 暂无 |
+| npm                | 6.1.0 | 暂无 |
+| webpack                | 4.16.2 | 暂无 |
+
+## 功能
+
+| 路径 | 功能 | 技术说明 |
+|---------|--------|-------------|
+| /          | 首页 | 无 |
+
+## 功能说明
+vue 跨域问题，未解决！
+
+## 运行方式
+请确保已经安装了node和webpack
+
+前端：
+进入目录 前端Vue\my-project
+
+运行命令:`
+```dos
+npm install
+```
+根据package.json会自动安装依赖包
+
+使用命令运行Vue项目
+```dos
+npm run dev
+```
+
+Copyright (c) 2018-present, xiao You
---
+++ mysite/src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <div id="app">
+    <button v-on:click="get_list">获取</button>
+    <div>{{ message }}</div>
+    <!--<img src="./assets/logo.png">-->
+    <!--<router-view/>-->
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+
+export default {
+  name: 'App',
+  data:function () {
+    return {
+      message:'',  //声明title变量
+      // content:''
+    }
+  },
+  methods:{   //定义方法对象
+    get_list:function(){
+      console.log(111);
+      // this.message = '111';
+      // console.log(this.HOST )
+      axios.get('/courses/thematic/')
+        .then((res)=> {
+          console.log('我要去后端拉取笔记数据啦！');
+          console.log(res.data.data);
+          messages = res.data.data;
+          // 将获取到的笔记数据 放到大仓库中
+          // _this.$store.state.noteList = res.data
+          // context.commit('INITNOTELIST', res.data.data)
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+  },
+  // computed: {
+  //     // 计算属性的 getter
+  //     reversedMessage: function () {
+  //       // `this` 指向 vm 实例
+  //       axios.get('/courses/thematic/')
+  //         .then((res)=> {
+  //           console.log('我要去后端拉取笔记数据啦！');
+  //           console.log(res.data.data)
+  //           // 将获取到的笔记数据 放到大仓库中
+  //           // _this.$store.state.noteList = res.data
+  //           // context.commit('INITNOTELIST', res.data.data)
+  //         })
+  //         .catch(function (error) {
+  //           console.log(error);
+  //         });
+  //     }
+  // },
+}
+</script>
+
+<style>
+/*#app {*/
+  /*font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
+  /*-webkit-font-smoothing: antialiased;*/
+  /*-moz-osx-font-smoothing: grayscale;*/
+  /*text-align: center;*/
+  /*color: #2c3e50;*/
+  /*margin-top: 60px;*/
+/*}*/
+</style>
